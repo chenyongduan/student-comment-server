d38de829f4e444f6fbd0f79550e4964f320e323b
chenyongduan <chenyongduan@97kid.com>
2017-09-29 10:36:07 +0800

    生成csv修改

--- routes/index.js
+++ routes/index.js
@@ -3,44 +3,25 @@ var router = express.Router();
 var fs = require('fs');
 var nodeExcel = require('excel-export');
 
-function writeXls(datas) {
-    var confs = [];
-    var conf = {};
-    conf.cols = [{
-        caption: 'string',
-        type: 'string'
-    },
-	{
-		caption: 'date',
-		type: 'date'
-	},
-	{
-		caption: 'bool',
-		type: 'bool'
-	},
-	{
-		caption: 'number 2',
-		type: 'number'
-	}];
-    conf.rows = [['hahai', (new Date(Date.UTC(2013, 4, 1))).oaDate(), true, 3.14], ["e", (new Date(2012, 4, 1)).oaDate(), false, 2.7182], ["M&M<>'", (new Date(Date.UTC(2013, 6, 9))).oaDate(), false, 1.2], ["null", null, null, null]];
-    for (var i = 0; i < 3; i++) {
-        conf = JSON.parse(JSON.stringify(conf));   //clone
-        conf.name = 'sheet'+i;
-        confs.push(conf);
-    }
-    var result = nodeExcel.execute(confs);
-	fs.writeFileSync("public/Group.csv", result, 'binary');
+function writeXls(studentInfo) {
+	var confs = [];
+	const conf = JSON.parse(studentInfo);
+	conf.name = 'sheet01';
+	confs.push(conf);
+	var result = nodeExcel.execute(confs);
+	fs.writeFileSync("public/student.csv", result, 'binary');
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	writeXls('newdata');
   res.render('index', { title: 'Express' });
 });
 
 router.post('/uploadStudent', function(req, res, next) {
-  console.log(req);
-  // writeXls('newdata');
+	const { studentInfo } = req.body;
+	if (studentInfo) {
+    writeXls(studentInfo);
+	}
 });
 
 module.exports = router;
